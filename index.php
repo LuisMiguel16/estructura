b1c928258952032a5e5647e6a4c2eeaa0805d5aa
LuisMiguel16 <50034509+LuisMiguel16@users.noreply.github.com>
2019-04-26 19:35:44 -0500

    cabeza de pagina

--- index.php
+++ index.php
@@ -1,8 +1,46 @@
 <?php
 	//php es un lenguaje d expresiones
 	$cantidad=0;
-	if($cantidad)
-		echo "Este mensaje es si la expresion devuelve true";
+	$nombres="";
+
+	if($nombres)
+	{
+
+		/*echo "Este mensaje es si la expresion devuelve true";*/
+
+
+	} //expresiones
 
 
 ?>
+
+<table>
+	<tr>
+		<td>Nombres</td>
+		<td>Apellidos</td>
+		<td>DNI</td>
+	</tr>
+	<?php if(1):?>
+		<style type="text/css">
+			td{
+				background: green;
+				color:white;
+			}
+
+		</style>
+		<tr>
+			<td>luis</td>
+			<td>Cabrera</td>
+			<td>12345678</td>
+		</tr>
+
+		<tr>
+			<td>Juan</td>
+			<td>Huaroto</td>
+			<td>87654321</td>
+
+		</tr>
+<?php endif;?>
+
+
+</table>
