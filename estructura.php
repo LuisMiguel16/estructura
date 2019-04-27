19b3116b5c936b8a224f23db35ea3ca14aaeda4c
LuisMiguel16 <50034509+LuisMiguel16@users.noreply.github.com>
2019-04-26 20:15:33 -0500

    Create estructura.php

---
+++ estructura.php
@@ -0,0 +1,28 @@
+<?php  
+	$monto=1000;//indicar si el monto tiene descuento (monto > 1000)
+	$edad;//indicar si es mayor o menor de edad
+	$sueldo;//indicar si el sueldo es minimo o no
+	/*if (true)
+	{
+		echo "Mensaje por ser true";
+	}else
+
+	{
+		echo "Mensaje por ser false";
+	}
+	echo "<br />";
+
+	echo false?"Mensaje por ser true":/* :en caso contrario*/
+
+
+	if ($monto>=1000)
+	{
+		echo "Si el monto es mayor de 1000 tiene descuento";
+	}else 
+	{
+		echo "Si el monto es menor de 1000 no tendra descuento";
+	}
+
+
+
+?>
